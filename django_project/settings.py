5482368c4c694524e3945f308f2b03782cf677d8
Mothusi Maemo <maemomothusi@gmail.com>
2022-08-06 02:05:19 -0700

    settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'students',
+    'students.apps.StudentsConfig',
 ]
 
 MIDDLEWARE = [
